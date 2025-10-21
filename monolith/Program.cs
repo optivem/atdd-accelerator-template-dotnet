cbb25437b58110658704888f5791e2dc83151c6c
Valentina Jemuović <valentina.jemuovic@optivem.com>
2025-10-21 11:07:53 +0200

    Add Echo and Todos API controllers for handling requests

--- monolith/Program.cs
+++ monolith/Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorPages();
+builder.Services.AddControllers();
 builder.Services.AddHttpClient();
 
 var app = builder.Build();
@@ -18,27 +19,6 @@ app.UseRouting();
 app.UseAuthorization();
 
 app.MapRazorPages();
-
-// Add API endpoint for testing
-app.MapGet("/api/echo", () => "Hello from API!");
-
-// Add todos API endpoint for E2E testing
-app.MapGet("/api/todos/{id:int}", async (int id) =>
-{
-    var baseUrl = app.Configuration["ExternalApis:JsonPlaceholder"] ?? "https://jsonplaceholder.typicode.com";
-    
-    using var httpClient = new HttpClient();
-    var response = await httpClient.GetAsync($"{baseUrl}/todos/{id}");
-    
-    if (response.IsSuccessStatusCode)
-    {
-        var jsonContent = await response.Content.ReadAsStringAsync();
-        return Results.Content(jsonContent, "application/json");
-    }
-    
-    return Results.NotFound();
-});
+app.MapControllers();
 
 app.Run();
-
-// TODO: VJ: DELETE COMMENT 2025-10-08
